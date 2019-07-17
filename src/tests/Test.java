d1f488bbbbefdabd2bf6b275f2fd39d4d0b91898
Maria <mariapaquin@u.boisestate.edu>
2019-07-17 14:07:02 -0600

    control flow implemented for if/then stmt

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -15,8 +15,18 @@ public class Test {
 //        }
 //    }
 
+//    public void m(){
+//        {
+//            int a = 1+1;
+//        }
+//        int b = 2+2;
+//    }
+
     public void m(){
-        int a = 1+1;
-        int b = 2+2;
+        int a = 1;
+        if(true){
+            int b = 2;
+        }
+        int c = 3;
     }
 }
--- src/visitor/ConstraintVisitor.java
+++ src/visitor/ConstraintVisitor.java
@@ -1,8 +1,16 @@
 package visitor;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
+import Constraint.Constraint;
+import Constraint.Operator.SubsetOperator;
+import Constraint.Term.ConstraintTerm;
+import Constraint.Term.EntryUnionAE;
+import Constraint.Term.ExpressionLiteral;
 import ConstraintCreator.ConstraintCreator;
+import ConstraintCreator.ConstraintTermFactory;
 import org.eclipse.jdt.core.dom.*;
 
 /**
@@ -12,97 +20,193 @@ import org.eclipse.jdt.core.dom.*;
  */
 public class ConstraintVisitor extends ASTVisitor {
 	private ConstraintCreator creator;
-
-
 	private HashSet constraints = new HashSet();
+	private ConstraintTermFactory variableFactory;
+	private List<ASTNode> prev;
 
 	public ConstraintVisitor(ConstraintCreator creator) {
 		this.creator = creator;
+		variableFactory = new ConstraintTermFactory();
+		prev = new ArrayList<>();
 	}
 
-	@Override
-	public  void endVisit(Block node) {
-		// adding Constraint RD_exit[S] \sub RD_entry[S']
-		//
-		// for S' following S in the block
-		// (after we have already generated the terms RD_exit[S] and RD_entry[S']
-		constraints.addAll(creator.createConstraints(node));
-	}
+//	@Override
+//	public boolean visit(EnhancedForStatement node) {
+//		return true;
+//	}
+//
+//	@Override
+//	public void endVisit(EnhancedForStatement node) {
+//		List<Constraint> result = new ArrayList<Constraint>();
+//		constraints.addAll(result);
+//	}
+//
+//	@Override
+//	public boolean visit(ForStatement node) {
+//		return true;
+//	}
+//
+//	@Override
+//	public void endVisit(ForStatement node) {
+//		List<Constraint> result = new ArrayList();
+//		constraints.addAll(result);
+//	}
 
 	@Override
-	public void endVisit(EnhancedForStatement node) {
-		constraints.addAll(creator.createConstraints(node));
-	}
+	public boolean visit(IfStatement node) {
+		List<Constraint> result = new ArrayList<Constraint>();
 
-	@Override
-	public void endVisit(ForStatement node) {
-		constraints.addAll(creator.createConstraints(node));
+		ConstraintTerm entry = variableFactory.createEntryLabel(node);
+		ConstraintTerm exit = variableFactory.createExitLabel(node);
+
+		if (!prev.isEmpty()) {
+			for (ASTNode p : prev) {
+				result.add(newSubsetConstraint(entry, variableFactory.createExitLabel(p)));
+			}
+		}
+
+		constraints.addAll(result);
+
+		prev.clear();
+		prev.add(node);
+
+		return true;
 	}
 
 	@Override
 	public void endVisit(IfStatement node) {
-		constraints.addAll(creator.createConstraints(node));
+		List<Constraint> result = new ArrayList<Constraint>();
+
+		ConstraintTerm entryUnionAE = variableFactory.createEntryUnionLabel(node);
+		ConstraintTerm entry = variableFactory.createEntryLabel(node);
+		ConstraintTerm exit = variableFactory.createExitLabel(node);
+
+		if (entryUnionAE != null) {
+			// the statement created at least one expression
+			result.add(newSubsetConstraint(exit, entryUnionAE));
+		} else {
+			result.add(newSubsetConstraint(exit, entry));
+		}
+
+		constraints.addAll(result);
+
+		Statement thenStmt = node.getThenStatement();
+		// TODO
+		// Statement elseStmt = node.getElseStatement();
+
+		prev.clear();
+		
+		if(thenStmt instanceof Block){
+			List<Statement> blockStmts = ((Block) thenStmt).statements();
+			if(!blockStmts.isEmpty()){
+				prev.add(blockStmts.get(blockStmts.size() - 1));
+			}
+		} else {
+			prev.add(thenStmt);
+		}
+
+		prev.add(node);
+
 	}
 
 	@Override
 	public boolean visit(InfixExpression node) {
-		creator.createTerms(node);
+		ExpressionLiteral expr = variableFactory.createExpressionLiteral(node);
+
+		ASTNode parentStmt = node.getParent();
+
+		while(!(parentStmt instanceof Statement)){
+			parentStmt = parentStmt.getParent();
+		}
+
+		EntryUnionAE parentStmtEntryUnion = variableFactory.createEntryUnionLabel(parentStmt);
+		parentStmtEntryUnion.addExpression(expr);
 		return true;
 	}
 
 
 	@Override
 	public boolean visit(MethodInvocation node) {
-		creator.createTerms(node);
+		if (node.getParent() instanceof ExpressionStatement) {
+			variableFactory.createEntryLabel(node.getParent());
+			variableFactory.createExitLabel(node.getParent());
+		}
 		return true;
 	}
 
 	@Override
 	public void endVisit(MethodInvocation node) {
-		constraints.addAll(creator.createConstraints(node));
+		List<Constraint> result = new ArrayList<Constraint>();
+
+		if (node.getParent() instanceof ExpressionStatement) {
+
+			ConstraintTerm entryUnionAE = variableFactory.createEntryUnionLabel(node);
+			ConstraintTerm entry = variableFactory.createEntryLabel(node);
+			ConstraintTerm exit = variableFactory.createExitLabel(node);
+
+			if (entryUnionAE != null) {
+				// the statement created at least one expression
+				result.add(newSubsetConstraint(exit, entryUnionAE));
+			} else {
+				result.add(newSubsetConstraint(exit, entry));
+			}
+		}
+		constraints.addAll(result);
 	}
 
-	@Override
-	public boolean visit(PostfixExpression node) {
-		creator.createTerms(node);
-		return true;
-	}
 
-	@Override
-	public boolean visit(PrefixExpression node) {
-		creator.createTerms(node);
-		return true;
-	}
-
-
-	@Override
-	public void endVisit(SwitchStatement node) {
-		constraints.addAll(creator.createConstraints(node));
-	}
-
-	@Override
-	public boolean visit(VariableDeclarationExpression node) {
-		constraints.addAll(creator.createConstraints(node));
-		return true;
-	}
+//	@Override
+//	public void endVisit(SwitchStatement node) {
+//		List<Constraint> result = new ArrayList<Constraint>();
+//		constraints.addAll(result);
+//	}
 
 	@Override
 	public boolean visit(VariableDeclarationStatement node) {
-		creator.createTerms(node);
+		variableFactory.createEntryLabel(node);
+		variableFactory.createExitLabel(node);
 		return true;
 	}
 
 	@Override
 	public void endVisit(VariableDeclarationStatement node) {
-		constraints.addAll(creator.createConstraints(node));
+		List<Constraint> result = new ArrayList<Constraint>();
+
+		ConstraintTerm entryUnionAE = variableFactory.createEntryUnionLabel(node);
+		ConstraintTerm entry = variableFactory.createEntryLabel(node);
+		ConstraintTerm exit = variableFactory.createExitLabel(node);
+
+		if (entryUnionAE != null) {
+			// the statement created at least one expression
+			result.add(newSubsetConstraint(exit, entryUnionAE));
+		} else {
+			result.add(newSubsetConstraint(exit, entry));
+		}
+
+		if (!prev.isEmpty()) {
+			for (ASTNode p : prev) {
+				result.add(newSubsetConstraint(entry, variableFactory.createExitLabel(p)));
+			}
+		}
+
+		prev.clear();
+		prev.add(node);
+
+		constraints.addAll(result);
 	}
 
-	@Override
-	public void endVisit(WhileStatement node) {
-		constraints.addAll(creator.createConstraints(node));
-	}
+//	@Override
+//	public void endVisit(WhileStatement node) {
+//		List<Constraint> result = new ArrayList<Constraint>();
+//		constraints.addAll(result);
+//	}
 
 	public HashSet getConstraints() {
 		return constraints;
+	}
+
+
+	public Constraint newSubsetConstraint(ConstraintTerm l, ConstraintTerm r) {
+		return new Constraint(l, new SubsetOperator(), r);
 	}
 }
